5d4f067df15ed8936dd59fba043243457edb59e2
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 11:24:19 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - A programm that prints with print functions
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
