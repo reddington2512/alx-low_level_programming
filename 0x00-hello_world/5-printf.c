35af4c90822f2ec928a0186e828f925e027ccf56
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 12:10:54 -0800

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - A programm that prints with print functions
- *
- * return: 0 when success
+ * main - A programm that prints a line using printf function
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		return (0);
 }
