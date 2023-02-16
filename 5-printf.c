b4f0b7e64c43822c67b2c2d053709cf0b3355cbd
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 13:19:37 -0800

    size of

---
+++ 5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - a programm that writes the size of computer
+ * retur: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a lon long int: %lu byte(s)\n" sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float;
+				return (0);
