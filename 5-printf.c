afb61623e210d6c9859104e8d65aba2b500c9c36
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 13:32:56 -0800

    size

--- 5-printf.c
+++ 5-printf.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - a programm that writes the size of computer
- * retur: 0
+ * return: 0
  */
 int main(void)
 {
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a lon long int: %lu byte(s)\n" sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float;
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 				return (0);
-				}
+}
