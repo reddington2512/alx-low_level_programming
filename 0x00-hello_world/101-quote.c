3ab5999272c1c51698229ce9c0f2c381c0de3624
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 21:00:03 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
- * Return 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by new line to standard error.
+ * Return: Always 1.
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
