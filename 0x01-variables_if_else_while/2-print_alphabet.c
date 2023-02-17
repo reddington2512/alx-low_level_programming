916b35e8dcea5c6f7f91f7cfcb139a5a65d06890
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-17 09:40:34 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int maun(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
