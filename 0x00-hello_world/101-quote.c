5fd9f2349c76b1d1b9bda80bdba463876288bf54
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 16:18:29 -0800

    qupte

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * *main - A c program that prints a line to the standard error
- * Return 1
+ * main - mEntry point
+ * void: When value X is void, it means it is not accepting any arg (*Descrription - Prints out the last part  of a quote in the standard error.)?
+ *
+ * (*unistd header: has the function write)*
+ * Return: 1
  */
-int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
