7990cbd645abd0547a28ce503e2ea0064a0a9e66
Emmanuel <emmanueladjeikrop@gmail.com>
2023-02-16 16:28:34 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - mEntry point
- * void: When value X is void, it means it is not accepting any arg (*Descrription - Prints out the last part  of a quote in the standard error.)?
- *
- * (*unistd header: has the function write)*
- * Return: 1
+ * main - Entry point
+ * Return 1
  */
+int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
